526f5d2254870c524a298b71625b86467139a045
GabrielLuizSF <gabrielluizads123@gmail.com>
2022-08-08 21:16:27 -0300

    server

---
+++ server/server.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+"log"
+"github.com/gin-gonic/gin"
+"github.com/GabrielLuizSF/GoLang-REST-API/server/routes"
+
+)
+
+type Server struct {
+	port string
+	server *gin.Engine
+}
+
+func NewServer() Server{
+	return Server{
+		port: "8500",
+		server: gin.Default(),
+	}
+}
+func (serverInit *Server) ServerRun(){
+	router := routes.ConfigRoutes(serverInit.server)
+
+	log.Printf("Server runing at port :  %v",serverInit.port)
+    log.Fatal(router.ServerRun(":"+serverInit.port))
+
+}
