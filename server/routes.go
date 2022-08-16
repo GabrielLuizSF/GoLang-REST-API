1c559d0deac349298bdc92cb6e8e57deecb8d6c6
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-08-15 21:37:39 -0300

    Create routes.go

---
+++ server/routes.go
@@ -0,0 +1,16 @@
+package server
+
+
+import (
+	
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"user_api-golang/controllers"
+)
+func initaliseHandlers(router *mux.Router) {
+	router.HandleFunc("/api/users", controllers.GetAllUser).Methods("GET")
+	router.HandleFunc("/api/user/create", controllers.CreateUser).Methods("POST")
+	router.HandleFunc("/api/user/{id}", controllers.GetUserByID).Methods("GET")
+	router.HandleFunc("/api/user/edit/{id}", controllers.UpdateUserByID).Methods("PUT")
+	router.HandleFunc("/api/user/delete/{id}", controllers.DeleteUserByID).Methods("DELETE")
+}
