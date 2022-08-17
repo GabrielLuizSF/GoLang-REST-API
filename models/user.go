5b5fa384be6d6d4beaa95fd53a0b2bda1a602b35
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-08-17 00:05:11 -0300

    remove age

--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
+
 type User struct {
 	ID        int    `json:"id"`
 	FULLName  string `json:"full_name"`
-	Age       int    `json:"age"`
 	AvatarURL string `json: "avatar_url"`
 	IsAdmin   bool   `json: "is_admin"`
 }
